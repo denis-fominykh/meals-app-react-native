fdb4925db597b9b9f20c8d884de8bd52bd0ae78a
denis-fominykh <fominykhdenis@gmail.com>
2020-07-17 23:47:14 +0300

    adding custom fonts to the project

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+
+import { fetchFonts } from './services/fetchFontsServices';
 
 const App: FC = () => {
+  const [fontLoaded, setFontLoaded] = useState<boolean>(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(error: Error) => console.log(error)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>

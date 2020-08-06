018f9bc713685db16b49a5921c58412ec0d88fe8
denis-fominykh <fominykhdenis@gmail.com>
2020-08-06 16:17:21 +0300

    adding styles to the categories screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import 'react-native-gesture-handler';
 import React, { FC } from 'react';
+import { StatusBar, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import Colours from './constants/Colours';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import CategoryMealsScreen from './screens/CategoryMealsScreen';
@@ -13,9 +16,27 @@ const Stack = createStackNavigator();
 const App: FC = () => {
   return (
     <SafeAreaProvider>
+      <StatusBar
+        backgroundColor={
+          Platform.OS === 'android' ? Colours.primaryColour : '#FFFFFF'
+        }
+        barStyle={Platform.OS === 'android' ? 'light-content' : 'dark-content'}
+      />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Categories" headerMode="none">
-          <Stack.Screen name="Categories" component={CategoriesScreen} />
+        <Stack.Navigator initialRouteName="Categories">
+          <Stack.Screen
+            name="Categories"
+            component={CategoriesScreen}
+            options={{
+              title: 'Meal Categories',
+              headerStyle: {
+                backgroundColor:
+                  Platform.OS === 'android' ? Colours.primaryColour : '#FFFFFF',
+              },
+              headerTintColor:
+                Platform.OS === 'android' ? '#FFFFFF' : Colours.primaryColour,
+            }}
+          />
           <Stack.Screen name="CategoryMeals" component={CategoryMealsScreen} />
           <Stack.Screen name="MealDetail" component={MealDetailScreen} />
         </Stack.Navigator>

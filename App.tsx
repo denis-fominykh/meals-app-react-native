d2cbbd0e54c02112dbea724a4cecb8a60374413d
denis-fominykh <fominykhdenis@gmail.com>
2020-08-09 13:32:44 +0300

    adding a paper provider

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React, { FC } from 'react';
 import { StatusBar, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import TabNavigator from './navigations/TabNavigator';
 import Colours from './constants/Colours';
@@ -10,15 +11,19 @@ import Colours from './constants/Colours';
 const App: FC = () => {
   return (
     <SafeAreaProvider>
-      <StatusBar
-        backgroundColor={
-          Platform.OS === 'android' ? Colours.primaryColour : '#FFFFFF'
-        }
-        barStyle={Platform.OS === 'android' ? 'light-content' : 'dark-content'}
-      />
-      <NavigationContainer>
-        <TabNavigator />
-      </NavigationContainer>
+      <PaperProvider>
+        <StatusBar
+          backgroundColor={
+            Platform.OS === 'android' ? Colours.primaryColour : '#FFFFFF'
+          }
+          barStyle={
+            Platform.OS === 'android' ? 'light-content' : 'dark-content'
+          }
+        />
+        <NavigationContainer>
+          <TabNavigator />
+        </NavigationContainer>
+      </PaperProvider>
     </SafeAreaProvider>
   );
 };

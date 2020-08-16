6d2cea82d667324d49ef2e295e1d9bc51174f988
denis-fominykh <fominykhdenis@gmail.com>
2020-08-17 00:12:37 +0300

    adding switches to filter screen

--- screens/FiltersScreen.tsx
+++ screens/FiltersScreen.tsx
@@ -1,10 +1,37 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
+import FilterSwitch from '../components/FilterSwitch';
+
 const FiltersScreen: FC = () => {
+  const [isGlutenFree, setIsGlutenFree] = useState<boolean>(false);
+  const [isLactoseFree, setIsLactoseFree] = useState<boolean>(false);
+  const [isVegan, setIsVegan] = useState<boolean>(false);
+  const [isVegetarian, setIsVegetarian] = useState<boolean>(false);
+
   return (
     <View style={styles.container}>
-      <Text>I&apos;m in the FiltersScreen component!</Text>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <FilterSwitch
+        label="Gluten-free"
+        state={isGlutenFree}
+        onChange={(newValue) => setIsGlutenFree(newValue)}
+      />
+      <FilterSwitch
+        label="Lactose-free"
+        state={isLactoseFree}
+        onChange={(newValue) => setIsLactoseFree(newValue)}
+      />
+      <FilterSwitch
+        label="Vegan"
+        state={isVegan}
+        onChange={(newValue) => setIsVegan(newValue)}
+      />
+      <FilterSwitch
+        label="Vegetarian"
+        state={isVegetarian}
+        onChange={(newValue) => setIsVegetarian(newValue)}
+      />
     </View>
   );
 };
@@ -14,7 +41,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    margin: 20,
+    textAlign: 'center',
   },
 });
 
